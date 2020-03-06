8c7a5830b515a77a8a9bdee7d8aa752df632cd99
Fabio <fabio.calcagno@fatec.sp.gov.br>
2020-03-06 14:16:13 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList
+} from 'react-native';
+
+
+import GoalInput from './components/GoalInput'
+import GoalItem from './components/GoalItem'
+
+export default function App() {
+ 
+  const [courseGoals, setCourseGoals] = useState([]);
+
+ 
+
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle }
+    ]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <GoalInput  onAddGoal={addGoalHandler}  />
+      <FlatList 
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={itemData => 
+          <GoalItem title={itemData.item.value} />
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  
+ 
+});
